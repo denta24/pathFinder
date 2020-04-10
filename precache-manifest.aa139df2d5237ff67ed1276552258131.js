c28de6c8bb3cb72cad618c12261c0ebd82bdf0ec
Dawid Kurpiel <kurpieldawid@gmail.com>
2020-04-10 19:07:09 +0200

    Updates

---
+++ precache-manifest.aa139df2d5237ff67ed1276552258131.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475827bfa5a2a8211cc837c407707eaa",
+    "url": "/denta24/pathFinder/index.html"
+  },
+  {
+    "revision": "d558de8e7f8fe04b5205",
+    "url": "/denta24/pathFinder/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "6d5fa60ca76bd0cf7629",
+    "url": "/denta24/pathFinder/static/css/main.6dfcd059.chunk.css"
+  },
+  {
+    "revision": "d558de8e7f8fe04b5205",
+    "url": "/denta24/pathFinder/static/js/2.e1acf33a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/denta24/pathFinder/static/js/2.e1acf33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5fa60ca76bd0cf7629",
+    "url": "/denta24/pathFinder/static/js/main.9317ad30.chunk.js"
+  },
+  {
+    "revision": "5f6231d54ac04c93e2ec",
+    "url": "/denta24/pathFinder/static/js/runtime-main.65460839.js"
+  }
+]);
